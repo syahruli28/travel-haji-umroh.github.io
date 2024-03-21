9436b747c0401e339cf86902ffb6ed78472df775
syahruli28 <syahruli917@gmail.com>
2024-03-21 14:09:40 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .flex .navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+
+
+// swiper js : reviews
+var swiper = new Swiper(".testimoni-slider", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        640: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+    },
+});
+// swiper js akhir
